7acb2c8bc468fe94d270b9de9f0d8693e322efea
Priscilla Baah <pyfbaah@gmail.com>
2023-01-12 23:37:15 +0000

    update with express logic

--- server.js
+++ server.js
@@ -1,105 +1,38 @@
-const EventEmitter = require('events');
+const express = require('express');
+const app = express();
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
 const fsPromises = require('fs').promises;
 
-const logEvents = require('./log-events');
-
-
-class Emitter extends EventEmitter {};
-const myEmitter = new Emitter(); 
+const logEvents = require('./middleware/log-events');
 
 const PORT = process.env.PORT || 5000;
 
-const serveFile = async (filePath, contentType, response) => {
-    try {
-        const data = await fsPromises.readFile(filePath, 'utf-8');
-        response.writeHead(200, {'Content-Type':contentType });
-        response.end(data)
+// custom middleware logger
+app.use( logger)
 
-    } catch (err) {
-        console.log(err);
-        response.statusCode = 500;
-        response.end();
-    }
-}
+// built-in middleware to handle urlencoded data, i.e. form data
+app.use(express.urlencoded({ extended: false}));
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+// built-in middleware for json
+app.use(express.json());
 
-    const extension = path.extname(req.url);
+// serve static files
+app.use(express.static(path.join(__dirname, 'views', 'static')))
 
-    let contentType;
-
-    switch (extension) {
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.jpg':
-            contentType = 'image/jpeg';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;
-        case '.txt':
-            contentType = 'text/plain';
-            break;
-        default:
-            contentType = 'text/html';
-    }
-
-
-    let filePath;
-
-    // if file is html and url is '/' get the home page
-    if ( contentType === 'text/html' && req.url === '/'){
-        filePath = path.join(__dirname, 'views', 'templates','home.html');
-    }
-
-    // if content type is html/text, find the file that was requested in the views/templates folder
-    else if (contentType === 'text/html'){
-        filePath = path.join(__dirname, 'views','templates', req.url);
-    }
-
-    // else use the directory name and the request url
-    else {
-        filePath = path.join(__dirname, req.url);
-    };
-    
-    // if there's no extension and the very last character of the request url
-    //  is not '/', add the .html extension
-    // makes the .html extension not required in the browser
-    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
-      
-    const fileExists = fs.existsSync(filePath);
-
-    if (fileExists) {
-        // serve the file
-        serveFile(filePath, contentType, res);
-    } else {
-        // 404
-        // 301 redirect
-        console.log(path.parse(filePath));
-    }
-
+app.get('^/$|/home(.html)?', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'views', 'templates', 'home.html'))
 });
 
-server.listen(PORT, () => {
+app.get('/shop(.html)?', (req, res)=> {
+    res.sendFile(path.join(__dirname, 'views', 'templates', 'shop.html'))
+})
+
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', 'templates', '404.html'))
+})
+
+app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
 });
-
-
-
-// myEmitter.on('log', (msg) => logEvents(msg)); // add listener to the log event
-
-// setTimeout(()=> {
-//     //Emit event
-//     myEmitter.emit('log', 'Log event emitted!');
-// }, 2000);
